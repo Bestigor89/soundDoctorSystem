986412e2ec8230740a82b01c1495ff6c82cab32d
igor Dudnek <igordudnek@gmail.com>
2022-08-30 22:27:22 +0300

    manager template

--- resources/views/livewire/manager/index.blade.php
+++ resources/views/livewire/manager/index.blade.php
@@ -1,11 +1,18 @@
 <div>
     <div class="card-controls sm:flex">
-
+    <input type="text" wire:model.debounce.300ms="searchQuery" name="searchQuery" id="searchQuery" class="w-full sm:w-1/3 inline-block">
     </div>
 
 
-    <div class="overflow-hidden">
+    <div class="overflow-hidden"  >
 
+
+        @foreach($pacients as $pacient)
+        <div>
+            {{$pacient->getName()}}
+        </div>
+
+        @endforeach
     </div>
 
     <div class="card-body">
