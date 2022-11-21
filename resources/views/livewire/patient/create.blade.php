c68c1f7db035a716c877d4d6ad2f50c6c7e1619a
panchenko91 <panchenko.vitaliy@gmail.com>
2022-11-21 23:33:59 +0200

    patient

--- resources/views/livewire/patient/create.blade.php
+++ resources/views/livewire/patient/create.blade.php
@@ -1,28 +1,50 @@
 <form wire:submit.prevent="submit" class="pt-3">
 
-    <div class="form-group {{ $errors->has('patient.name') ? 'invalid' : '' }}">
-        <label class="form-label required" for="name">{{ trans('cruds.patient.fields.name') }}</label>
-        <input class="form-control" type="text" name="name" id="name" required wire:model.defer="patient.name">
+    <div class="form-group {{ $errors->has('user.name') ? 'invalid' : '' }}">
+        <label class="form-label required" for="name">{{ trans('cruds.user.fields.name') }}</label>
+        <input class="form-control" type="text" name="name" id="name" required wire:model.defer="user.name">
         <div class="validation-message">
-            {{ $errors->first('patient.name') }}
+            {{ $errors->first('user.name') }}
         </div>
         <div class="help-block">
-            {{ trans('cruds.patient.fields.name_helper') }}
+            {{ trans('cruds.user.fields.name_helper') }}
         </div>
     </div>
-    <div class="form-group {{ $errors->has('patient.doctor_id') ? 'invalid' : '' }}">
-        <label class="form-label required" for="doctor">{{ trans('cruds.patient.fields.doctor') }}</label>
-        <x-select-list class="form-control" required id="doctor" name="doctor" :options="$this->listsForFields['doctor']" wire:model="patient.doctor_id" />
+    <div class="form-group {{ $errors->has('user.email') ? 'invalid' : '' }}">
+        <label class="form-label required" for="email">{{ trans('cruds.user.fields.email') }}</label>
+        <input class="form-control" type="email" name="email" id="email" required wire:model.defer="user.email">
         <div class="validation-message">
-            {{ $errors->first('patient.doctor_id') }}
+            {{ $errors->first('user.email') }}
         </div>
         <div class="help-block">
-            {{ trans('cruds.patient.fields.doctor_helper') }}
+            {{ trans('cruds.user.fields.email_helper') }}
         </div>
     </div>
+    <div class="form-group {{ $errors->has('password') ? 'invalid' : '' }}">
+        <label class="form-label required" for="password">{{ trans('cruds.user.fields.password') }}</label>
+        <input class="form-control" type="password" name="password" id="password" required wire:model.defer="password">
+        <div class="validation-message">
+            {{ $errors->first('password') }}
+        </div>
+        <div class="help-block">
+            {{ trans('cruds.user.fields.password_helper') }}
+        </div>
+    </div>
+
+    <div class="form-group {{ $errors->has('user.status') ? 'invalid' : '' }}">
+        <input class="form-control" type="checkbox" name="status" id="status" required wire:model.defer="user.status">
+        <label class="form-label inline ml-1 required" for="status">{{ trans('cruds.user.fields.status') }}</label>
+        <div class="validation-message">
+            {{ $errors->first('user.status') }}
+        </div>
+        <div class="help-block">
+            {{ trans('cruds.user.fields.status_helper') }}
+        </div>
+    </div>
+
     <div class="form-group {{ $errors->has('patient.phone') ? 'invalid' : '' }}">
         <label class="form-label" for="phone">{{ trans('cruds.patient.fields.phone') }}</label>
-        <input class="form-control" type="text" name="phone" id="phone" wire:model.defer="patient.phone">
+        <input class="form-control" type="text" name="phone" id="phone" wire:model.defer="phone">
         <div class="validation-message">
             {{ $errors->first('patient.phone') }}
         </div>
@@ -30,24 +52,15 @@
             {{ trans('cruds.patient.fields.phone_helper') }}
         </div>
     </div>
-    <div class="form-group {{ $errors->has('patient.status') ? 'invalid' : '' }}">
-        <input class="form-control" type="checkbox" name="status" id="status" wire:model.defer="patient.status">
-        <label class="form-label inline ml-1" for="status">{{ trans('cruds.patient.fields.status') }}</label>
+
+    <div class="form-group {{ $errors->has('patient.doctor_id') ? 'invalid' : '' }}">
+        <label class="form-label required" for="doctor">{{ trans('cruds.patient.fields.doctor') }}</label>
+        <x-select-list class="form-control" required id="doctor" name="doctor" :options="$this->listsForFields['doctor']" wire:model="doctor_id" />
         <div class="validation-message">
-            {{ $errors->first('patient.status') }}
+            {{ $errors->first('patient.doctor_id') }}
         </div>
         <div class="help-block">
-            {{ trans('cruds.patient.fields.status_helper') }}
-        </div>
-    </div>
-    <div class="form-group {{ $errors->has('patient.user_id') ? 'invalid' : '' }}">
-        <label class="form-label required" for="user">{{ trans('cruds.patient.fields.user') }}</label>
-        <x-select-list class="form-control" required id="user" name="user" :options="$this->listsForFields['user']" wire:model="patient.user_id" />
-        <div class="validation-message">
-            {{ $errors->first('patient.user_id') }}
-        </div>
-        <div class="help-block">
-            {{ trans('cruds.patient.fields.user_helper') }}
+            {{ trans('cruds.patient.fields.doctor_helper') }}
         </div>
     </div>
 
